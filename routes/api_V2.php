d82ca59b608a8e81a42dbb49ee158995e6b0d54f
Krotov Anatoliiy <tolumbas@gmail.com>
2021-12-18 23:26:11 +0200

    chore: Updated formatting

--- routes/api_V2.php
+++ routes/api_V2.php
@@ -21,5 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //Route::get('/desks',[\App\Http\Controllers\Api\DeskController::class, 'index']);
 
 Route::apiResources(
-['desks' => DeskController::class]
+	['desks' => DeskController::class]
 );
